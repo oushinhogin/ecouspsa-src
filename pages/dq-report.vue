88e960cc2083760606e3897a783ec2154e37d47e
Josh Horner <josh@bkmediagroup.com>
2019-08-22 08:56:12 -0600

    adds a dq report page

---
+++ pages/dq-report.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfxE5Us6ZLchZA5pX3Nzt4neDrNWfiA-WdZUHdzRtxPWxVQwQ/viewform?embedded=true"
+        width="640"
+        height="1187"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0">Loading…</iframe>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  iframe {
+    margin: 0px auto;
+    display: block;
+  }
+</style>
