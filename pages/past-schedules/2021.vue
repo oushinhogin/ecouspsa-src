b050a0f89be4e1fc3825f283f4d5bf58f817c714
Josh Horner <josh@bkmediagroup.com>
2021-12-01 22:37:30 -0700

    2022 schedule

---
+++ pages/past-schedules/2021.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <div class="is-centered">
+          <h1 class="title">USPSA Matches {{year}}</h1>
+          <h2 class="subtitle">
+            (past schedules:
+            <nuxt-link to="/past-schedules/2018">2018</nuxt-link>,
+            <nuxt-link to="/past-schedules/2019">2019</nuxt-link>,
+            <nuxt-link to="/past-schedules/2020">2020</nuxt-link>)
+          </h2>
+        </div>
+
+        <schedule-table :year="year" />
+
+          <div class="notification is-warning">
+            All early spring, late fall, and winter matches are subject to
+            advance cancelation due to range conditions, weather, or weather
+            forecasts. Keep an eye on the <nuxt-link
+                                            to="results#googlegroup">Google Group</nuxt-link> for updates.
+          </div>
+          <div class="content">
+            <h4>Colorado Major Matches</h4>
+            <ol>
+              <li>6/4/21 - 6/6/21 Rocky Mountain 300 Weld, CO</li>
+              <li>6/25/21 - 6/27/21 Colorado State - Big Horn Classic Grand Junction, CO</li>
+              <li>7/16/21 - 7/18/21 Mile High Showdown Ramah, CO</li>
+            </ol>
+            <h4>USPSA Nationals</h4>
+            <ul>
+              <li>5/7/21 - 5/9/21 LoCap Nationals (Prod, SS, L10, Revo) Talladega, AL</li>
+              <li>10/16/21 - 10/18/21 Carry Optics / PCC Nationals Talladega, AL</li>
+              <li>10/22/21 - 10/24/21 Limited/Open Nationals Talladega, AL</li>
+            </ul>
+            <h4>Nearby Area Matches</h4>
+            <ul>
+              <li>7/30/21 - 8/1/21 Area 3 Championship Grand Island, NE</li>
+              <li>8/6/21 - 8/8/21 Area 1 Championship Puyallup, WA</li>
+              <li>11/11/21 - 11/14/21 Area 2 Championship Mesa, AZ</li>
+            </ul>
+          </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+
+<script>
+import ScheduleTable from '~/components/schedule/Table.vue'
+export default {
+  components: {
+    ScheduleTable
+  },
+  data () {
+    return {
+      year: 2021,
+    }
+  }
+}
+</script>
+
+<style lang="scss" scopped>
+.is-centered {
+  margin-bottom: 30px;
+}
+.key {
+  display: flex;
+  justify-content: center;
+  table {
+    margin: 0 20px;
+  }
+
+}
+.more-info {
+  margin-bottom: 25px;
+}
+.additional-club-info {
+  @extend .column;
+  flex-basis: 25%;
+}
+.tab-content {
+  margin-bottom: 50px;
+}
+</style>
---
+++ pages/schedule-2022.vue
@@ -0,0 +1,200 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <div class="is-centered">
+          <h1 class="title">USPSA Matches {{year}}</h1>
+          <h2 class="subtitle">
+            (past schedules:
+            <nuxt-link to="/past-schedules/2019">2019</nuxt-link>,
+            <nuxt-link to="/past-schedules/2020">2020</nuxt-link>,
+            <nuxt-link to="/past-schedules/2021">2021</nuxt-link>)
+          </h2>
+        </div>
+
+        <schedule-table :year="year" />
+
+          <div class="notification is-warning">
+            All early spring, late fall, and winter matches are subject to
+            advance cancelation due to range conditions, weather, or weather
+            forecasts. Keep an eye on the <nuxt-link
+                                            to="results#googlegroup">Google Group</nuxt-link> for updates.
+          </div>
+          <div class="content">
+            <h2>Major USPSA Matches</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Dates</th>
+                  <th>Match Name</th>
+                  <th>Location</th>
+                  <th>Fee</th>
+                  <th>When Does Reg Open?</th>
+                  <th>On PS?</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr>
+                  <td>3/30 - 4/3</td>
+                  <td>Multigun Nationals</td>
+                  <td>Clinton, SC</td>
+                  <td>$295</td>
+                  <td>n/a</td>
+                  <td>N</td>
+                </tr>
+
+                <tr>
+                  <td>4/20 - 4/24</td>
+                  <td>Colorado State - Big Horn Classic</td>
+                  <td>Palisade, CO</td>
+                  <td>$150</td>
+                  <td>open now</td>
+                  <td>Y</td>
+                </tr>
+
+                <tr>
+                  <td>5/20 - 5/22</td>
+                  <td>Classic Nationals (SS, L10, Revo)</td>
+                  <td>Talladega, AL</td>
+                  <td>$250</td>
+                  <td>n/a</td>
+                  <td>N</td>
+                </tr>
+
+                <tr>
+                  <td>6/2 - 6/5</td>
+                  <td>Area 1 Championship</td>
+                  <td>Nampa, ID</td>
+                  <td>$200</td>
+                  <td>01/03</td>
+                  <td>Y</td>
+                </tr>
+
+                <tr>
+                  <td>6/9 - 6/12</td>
+                  <td>2-Gun PCC/Pistol Nationals</td>
+                  <td>Palisade, CO</td>
+                  <td>$295</td>
+                  <td>n/a</td>
+                  <td>N</td>
+                </tr>
+
+                <tr>
+                  <td>6/10 - 6/12</td>
+                  <td>Rocky Mountain 300</td>
+                  <td>Weld, CO</td>
+                  <td>$99</td>
+                  <td>01/23</td>
+                  <td>Y</td>
+                </tr>
+
+                <tr>
+                  <td>8/11 - 8/14</td>
+                  <td>Area 3 Championship</td>
+                  <td>Alda, NE</td>
+                  <td>n/a</td>
+                  <td>n/a</td>
+                  <td>N</td>
+                </tr>
+
+                <tr>
+                  <td>9/1 - 9/4</td>
+                  <td>Utah State</td>
+                  <td>Salt Lake City, UT</td>
+                  <td>$160</td>
+                  <td>12/05/21</td>
+                  <td>Y</td>
+                </tr>
+
+                <tr>
+                  <td>9/7 - 9/11</td>
+                  <td>Carry Optics Nationals</td>
+                  <td>Talladega, AL</td>
+                  <td>$285</td>
+                  <td>n/a</td>
+                  <td>N</td>
+                </tr>
+
+                <tr>
+                  <td>9/22 - 9/25</td>
+                  <td>Area 4 Championship</td>
+                  <td>Tulsa, OK</td>
+                  <td>$185</td>
+                  <td>n/a</td>
+                  <td>N</td>
+                </tr>
+
+                <tr>
+                  <td>9/23/2 - 9/25</td>
+                  <td>US IPSC Nationals</td>
+                  <td>Frostproof, FL</td>
+                  <td>$210</td>
+                  <td>n/a</td>
+                  <td>Y</td>
+                </tr>
+
+                <tr>
+                  <td>10/11 - 10/16</td>
+                  <td>Open/Limited/PCC/Production Nationals</td>
+                  <td>Palisade, CO</td>
+                  <td>$295</td>
+                  <td>n/a</td>
+                  <td>N</td>
+                </tr>
+
+                <tr>
+                  <td>11/10 - 11/13</td>
+                  <td>Area 2 Championship</td>
+                  <td>Mesa, AZ</td>
+                  <td>$330</td>
+                  <td>n/a</td>
+                  <td>N</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+
+<script>
+import ScheduleTable from '~/components/schedule/Table.vue'
+export default {
+  components: {
+    ScheduleTable
+  },
+  data () {
+    return {
+      year: 2022,
+    }
+  }
+}
+</script>
+
+<style lang="scss" scopped>
+.is-centered {
+  margin-bottom: 30px;
+}
+.key {
+  display: flex;
+  justify-content: center;
+  table {
+    margin: 0 20px;
+  }
+
+}
+.more-info {
+  margin-bottom: 25px;
+}
+.additional-club-info {
+  @extend .column;
+  flex-basis: 25%;
+}
+.tab-content {
+  margin-bottom: 50px;
+}
+</style>
+
